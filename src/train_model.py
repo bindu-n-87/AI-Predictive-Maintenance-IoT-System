7ba50a1303c9f1eda712ea405cd020c35a8cf3f6
bindu-n-87 <bindu.np18@gmail.com>
2026-04-14 03:39:57 +0530

    Trained Random Forest model

--- src/train_model.py
+++ src/train_model.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
 
-# -----------------------------
-# 1. Load Preprocessed Data
-# -----------------------------
+
 X_train = np.load("data/X_train.npy")
 X_test = np.load("data/X_test.npy")
 y_train = np.load("data/y_train.npy")
@@ -16,38 +14,24 @@ y_test = np.load("data/y_test.npy")
 
 print("Data Loaded Successfully")
 
-# -----------------------------
-# 2. Create Model
-# -----------------------------
 model = RandomForestClassifier(
     n_estimators=100,
     random_state=42
 )
 
-# -----------------------------
-# 3. Train Model
-# -----------------------------
+
 model.fit(X_train, y_train)
 
 print("\nModel Training Completed")
 
-# -----------------------------
-# 4. Predictions
-# -----------------------------
 y_pred = model.predict(X_test)
 
-# -----------------------------
-# 5. Evaluation
-# -----------------------------
 accuracy = accuracy_score(y_test, y_pred)
 
 print("\nAccuracy:", accuracy)
 print("\nClassification Report:\n")
 print(classification_report(y_test, y_pred))
 
-# -----------------------------
-# 6. Confusion Matrix
-# -----------------------------
 cm = confusion_matrix(y_test, y_pred)
 
 plt.figure(figsize=(6,4))
@@ -58,9 +42,6 @@ plt.ylabel("Actual")
 plt.savefig("outputs/confusion_matrix.png")
 plt.show()
 
-# -----------------------------
-# 7. Feature Importance
-# -----------------------------
 importances = model.feature_importances_
 features = ["temperature", "vibration", "pressure", "rpm", "wear_level"]
 
@@ -70,9 +51,6 @@ plt.title("Feature Importance")
 plt.savefig("outputs/feature_importance.png")
 plt.show()
 
-# -----------------------------
-# 8. Save Model
-# -----------------------------
 joblib.dump(model, "models/predictive_model.pkl")
 
 print("\nModel Saved Successfully!")
